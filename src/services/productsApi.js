216ca581857b09e053f8271bc5640d52caf079da
Luis <luis.sepulveda.villarroel@estudiantes.ipss.cl>
2026-01-12 10:19:36 -0300

    Creación de carpeta services con el enlace a la API

---
+++ src/services/productsApi.js
@@ -0,0 +1,12 @@
+const API_URL = "https://dummyjson.com/products";
+
+export async function getProducts() {
+  const response = await fetch(API_URL);
+
+  if (!response.ok) {
+    throw new Error("Error al obtener los productos");
+  }
+
+  const data = await response.json();
+  return data.products;
+}
