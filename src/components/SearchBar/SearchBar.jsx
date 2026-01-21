b24a7b19477006f2ad9a06672ae21ca02b9a236f
Luis <luis.sepulveda.villarroel@estudiantes.ipss.cl>
2026-01-21 00:55:06 -0300

    Arreglos esteticos en el SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react"; //función de React para manejar estados del componente
+import { useState } from "react";
 import "./SearchBar.css";
 
 //función que recibe componente onSearch
 function SearchBar({ onSearch }) {
-  const [value, setValue] = useState(""); //Se guarda lo que el usuario escribe en el buscador
+  const [value, setValue] = useState(""); 
 
-  const handleChange = (e) => { //Se ejecuta cada vez que el usuario escribe en el buscador
-    const text = e.target.value; //Se captura el texto actual
-    console.log("Buscando:", text); //Se muestra por consola lo que el usuario esta escribiendo por motivos de debugging
-    setValue(text); //Actualizamos el estado del input
-    onSearch(text); //Se envía el texto al componente padre
+  const handleChange = (e) => { 
+    const text = e.target.value; 
+    console.log("Buscando:", text); 
+    setValue(text); 
+    onSearch(text); 
   };
 
   return (
@@ -18,7 +18,7 @@ function SearchBar({ onSearch }) {
       placeholder="Busca tus productos o animes preferidos aquí"
       className="search-bar"
       value={value}
-      onChange={handleChange} //Evento del input cada vez que se modifica, borra o escribe texto
+      onChange={handleChange} 
     />
   );
 }
